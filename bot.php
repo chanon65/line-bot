a5f3f3039c74a6e6830822418f7a54907272419f
chanon65 <pronteradap65@gmail.com>
2017-12-13 10:28:24 +0700

    scscsc

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else{
+}else if($arrJson['events'][0]['message']['text'] <> ""){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
