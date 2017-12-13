6c6e7930c17c835a5027953410c4ae6022b6d0dd
chanon65 <pronteradap65@gmail.com>
2017-12-13 12:00:04 +0700

    ascasc

--- bot.php
+++ bot.php
@@ -6,6 +6,7 @@ $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
 $strUrl = "https://api.line.me/v2/bot/message/reply";
+$strUrlpush = "https://api.line.me/v2/bot/message/push";
  
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
@@ -34,12 +35,28 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
 
+$arrPushData = array();
+$arrPushData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
+$arrPushData['messages'][0]['type'] = "text";
+$arrPushData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrlpush);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPushData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
