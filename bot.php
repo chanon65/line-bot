502c69978e3a0e341a085c162a871821f04b1f28
chanon65 <pronteradap65@gmail.com>
2017-12-13 12:33:25 +0700

    ascsac

--- bot.php
+++ bot.php
@@ -45,20 +45,11 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode($arrPushData));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
 
-$ch1 = curl_init();
-curl_setopt($ch1, CURLOPT_URL,$strUrlpush);
-//curl_setopt($ch1, CURLOPT_HEADER, false);
-curl_setopt($ch1, CURLOPT_POST, true);
-curl_setopt($ch1, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode($arrPushData));
-curl_setopt($ch1, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch1);
-curl_close ($ch1);
- 
+
 ?>
