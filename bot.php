0ff5b53e31a70ff4e6cfdf243633953bab914f28
chanon65 <pronteradap65@gmail.com>
2017-12-13 12:55:44 +0700

    s

--- bot.php
+++ bot.php
@@ -1,14 +1,9 @@
 <?php
+include('bot2.php');
 $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
  
-
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
- 
-$strUrlPost = "https://api.line.me/v2/bot/message/reply";
 $strUrl = "https://api.line.me/v2/bot/message/push";
 
- 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
@@ -17,7 +12,6 @@ $arrPushData = array();
 $arrPushData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPushData['messages'][0]['type'] = "text";
 $arrPushData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
- 
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
@@ -30,22 +24,4 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-  
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrlPost);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
-}
- 
 ?>
