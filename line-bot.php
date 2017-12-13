ea5cb7c305b72c40d91c6dfabbef8632ad059d39
chanon65 <pronteradap65@gmail.com>
2017-12-13 07:57:41 +0700

    ทดสอบ bot

---
+++ line-bot.php
@@ -0,0 +1,25 @@
+<?php
+
+// example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
+
+include ('line-bot.php');
+
+$channelSecret = '801a3f00cf9218461e5826bc52d57f3b';
+$access_token  = '2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=';
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	if ($bot->isSuccess()) {
+		echo 'Succeeded!';
+		exit();
+	}
+
+	// Failed
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	exit();
+
+}
