760ecfc4c4409302c10678ab86fd4467105db953
chanon65 <pronteradap65@gmail.com>
2017-12-13 08:29:33 +0700

    ผปแผปแผปแ

--- bot.php
+++ bot.php
@@ -26,22 +26,21 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else if($arrJson['events'][0]['message']['text'] == "เย็นนี้ไปไหนดี"){
+ 
+}else if($arrJson['events'][0]['message']['text'] == "เย็นนี้ทำอะไรดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ตีหรี่กันไหม";
+ 
 }else{
   $arrPostData = array();
-  
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-
-
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+ echo $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+ echo $arrPostData['messages'][0]['type'] = "text";
+ echo $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
