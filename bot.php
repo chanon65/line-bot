7489ca9a8572112f6db6da7fe1ebf97b3ce0db2c
chanon65 <pronteradap65@gmail.com>
2017-12-13 09:38:43 +0700

    asdasd

--- bot.php
+++ bot.php
@@ -1,14 +1,15 @@
-<!--<meta http-equiv="refresh" content="10" > -->
 <?php
-session_start();
 $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
  
-$strUrl = "https://api.line.me/v2/bot/message/push";
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
  
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-
+ 
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -30,14 +31,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-
-//$arrPostData = array();
-//$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
-//$arrPostData['messages'][0]['type'] = "text";
-//$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm" . $_SESSION["count"];
-//$_SESSION["count"]++;
-//unset($arrPostData);
-
+ 
+ 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
