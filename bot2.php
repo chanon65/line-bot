b6100f36b5bec2ad247b79212ec382399beb5a73
chanon65 <pronteradap65@gmail.com>
2017-12-13 11:54:20 +0700

    ascasc

--- bot2.php
+++ bot2.php
@@ -1,13 +1,13 @@
 <meta http-equiv="refresh" content="20" > 
 <?php
  
-$strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
+//$strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
  
 $strUrl = "https://api.line.me/v2/bot/message/push";
  
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+//$arrHeader = array();
+//$arrHeader[] = "Content-Type: application/json";
+//$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
 $arrPostData = array();
 $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
@@ -15,15 +15,15 @@ $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
  
  
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
+//$ch = curl_init();
+//curl_setopt($ch, CURLOPT_URL,$strUrl);
+//curl_setopt($ch, CURLOPT_HEADER, false);
+//curl_setopt($ch, CURLOPT_POST, true);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//$result = curl_exec($ch);
+//curl_close ($ch);
  
 ?>
