5fe63e208d177a8b2a8659aa436de6967fc46953
chanon65 <pronteradap65@gmail.com>
2017-12-13 09:02:44 +0700

    ascasc

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <meta http-equiv="refresh" content="10" > 
 <?php
- 
+session_start();
 $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
  
 $strUrl = "https://api.line.me/v2/bot/message/push";
@@ -10,12 +10,12 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
 
-
 $i = 0;
+$_SESSION["number"] = $i;
 $arrPostData = array();
 $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm" . $i;
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm" . $_SESSION["number"];
 $i++;
  
 $ch = curl_init();
