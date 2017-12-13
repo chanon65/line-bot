883b2dd3986a635701903453df128121c860e0e6
chanon65 <pronteradap65@gmail.com>
2017-12-13 09:14:42 +0700

    asd

--- bot.php
+++ bot.php
@@ -1,4 +1,4 @@
-<meta http-equiv="refresh" content="10" > 
+<meta http-equiv="refresh" content="20" > 
 <?php
 session_start();
 $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
@@ -9,13 +9,11 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
-
-$i = 0;
-$_SESSION["number"] = $i++;
 $arrPostData = array();
 $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPostData['messages'][0]['type'] = "text";
-$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm" . $_SESSION["number"];
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm" . $_SESSION["count"];
+$_SESSION["count"]++;
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
