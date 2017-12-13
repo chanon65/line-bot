182c5e2692ff47e78dbc19f2410a8e53e6015c56
chanon65 <pronteradap65@gmail.com>
2017-12-13 14:03:19 +0700

    s

--- bot.php
+++ bot.php
@@ -32,6 +32,11 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
 		$arrPostData['messages'][0]['type'] = "text";
 		$arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+	} elseif($arrJson['events'][0]['message']['sticker'] > "0"){
+		$arrPostData = array();
+		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+		$arrPostData['messages'][0]['type'] = "text";
+		$arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
 	} else {
 		$arrPostData = array();
 		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
