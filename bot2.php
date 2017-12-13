44151e681e8793c8aad9f4569cccd6dc5c57c580
chanon65 <pronteradap65@gmail.com>
2017-12-13 13:01:35 +0700

    ห

--- bot2.php
+++ bot2.php
@@ -5,10 +5,6 @@ $arrJson = json_decode($content, true);
  
 $strUrl = "https://api.line.me/v2/bot/message/reply";
  
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
