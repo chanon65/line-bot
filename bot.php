196d3a295c2f694f0500b9ae9f1bcdcb77c32764
chanon65 <pronteradap65@gmail.com>
2017-12-13 13:57:04 +0700

    s

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@ $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
 
-if ($arrJson['events'][0]['message']['text'] <> "" || $arrJson['events'][0]['message']['sticker'] <> ""){
+if ($arrJson['events'][0]['message']['text'] <> ""){
 	$strUrl = "https://api.line.me/v2/bot/message/reply";
 if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 		$arrPostData = array();
@@ -36,7 +36,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 		$arrPostData = array();
 		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
 		$arrPostData['messages'][0]['type'] = "sticker";
-		$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+		$arrPostData['messages'][0]['packageId'] = "1";
+		$arrPostData['messages'][0]['stickerId'] = "1";
 	}
 }
  
