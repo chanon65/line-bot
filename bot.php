b4d9b3b9032b6a7c9e15846f7fd33927e119f86f
chanon65 <pronteradap65@gmail.com>
2017-12-14 11:52:03 +0700

    1

--- bot.php
+++ bot.php
@@ -39,6 +39,7 @@ if($arrJson['events'][0]['message']['text'] == ""){
 		$arrPostData['messages'][0]['type'] = "text";
 		$arrPostData['messages'][0]['text'] = "รูปภาพหรือสติกเกอร์";
 } else {
+		$strUrl = "https://api.line.me/v2/bot/message/reply";
 		$arrPostData = array();
 		$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 		$arrPostData['messages'][0]['type'] = "text";
