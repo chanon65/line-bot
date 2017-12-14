b8fc4ae3fcfe415af6790d5fd7e42b0342e0d571
chanon65 <pronteradap65@gmail.com>
2017-12-14 09:23:18 +0700

    s

--- bot.php
+++ bot.php
@@ -39,13 +39,6 @@ $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
 
-if ($arrJson['events'][0]['message']['sticker'] <> ""){
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
-
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
