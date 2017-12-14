ffa5031c0afa028c72afefec53370c8bb14a15f2
chanon65 <pronteradap65@gmail.com>
2017-12-14 14:37:40 +0700

    123

--- bot.php
+++ bot.php
@@ -39,8 +39,7 @@ if($arrJson['events'][0]['message']['text'] <> "" || $arrJson['events'][0]['mess
 		$arrPostData['messages'][0]['type'] = "text";
 		$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 	}
-} 
-
+} else {
 	$arrPostData = array();
 	$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 	$arrPostData['messages'][0]['type'] = "text";
