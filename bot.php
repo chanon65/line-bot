2baf92ba840d8da5cd8cf5316b16452c74dc5074
chanon65 <pronteradap65@gmail.com>
2017-12-14 15:10:57 +0700

    1231

--- bot.php
+++ bot.php
@@ -24,16 +24,11 @@ if($arrJson['events'][0]['message']['text'] <> ""){
 		$arrPostData['messages'][0]['type'] = "text";
 		$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 	}
-} elseif($arrJson['events'][0]['message']['text'] == "") {
-		$arrPostData = array();
-		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		$arrPostData['messages'][0]['type'] = "text";
-		$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 } else {
 	$arrPostData = array();
 	$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+	$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Alarm";
 }
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
