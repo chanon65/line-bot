f91fcd24d9db456658e7bc39761182018f792d64
chanon65 <pronteradap65@gmail.com>
2017-12-14 13:56:12 +0700

    111

--- bot.php
+++ bot.php
@@ -1,6 +1,8 @@
 <?php
 $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=";
 
+
+
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
@@ -11,13 +13,10 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
+
 if($arrJson['events'][0]['message']['text'] == ""){
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "อย่าโพสรูปและสติกเกอร์";
-}else if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 $strUrl = "https://api.line.me/v2/bot/message/reply";
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -41,8 +40,17 @@ $strUrl = "https://api.line.me/v2/bot/message/reply";
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
+	}
+} else 
 
+$strUrl = "https://api.line.me/v2/bot/message/push";
+
+$arrPostData = array();
+$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+ 
+}
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
