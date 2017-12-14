96ab1a6fa152ad04922a470f23df3279fe1933d4
chanon65 <pronteradap65@gmail.com>
2017-12-14 12:26:32 +0700

    1

--- bot.php
+++ bot.php
@@ -32,13 +32,6 @@ if ($arrJson['events'][0]['message']['text'] <> ""){
 	}
 } 
 
-if($arrJson['events'][0]['message']['text'] == ""){
-		$strUrl = "https://api.line.me/v2/bot/message/reply";
-		$arrPostData = array();
-		$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-		$arrPostData['messages'][0]['type'] = "text";
-		$arrPostData['messages'][0]['text'] = "รูปภาพหรือสติกเกอร์";
-} 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
