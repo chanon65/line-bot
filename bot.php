696f7e418c2d6c20cb7433ed01469bd6f309081c
chanon65 <pronteradap65@gmail.com>
2017-12-14 10:16:51 +0700

    ๅๅๅ

--- bot.php
+++ bot.php
@@ -1,9 +1,9 @@
 <?php
 curl -X POST \
 -H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
+-H 'Authorization: Bearer {2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=}' \
 -d '{
-    "to": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "to": "U961224e379af4062d4ce99f7e9c46dfe",
     "messages":[
         {
             "type":"text",
