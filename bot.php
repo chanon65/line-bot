fb3ad861ba63cd784e8d2eef1c586a04d1636435
chanon65 <pronteradap65@gmail.com>
2017-12-14 12:37:23 +0700

    1

--- bot.php
+++ bot.php
@@ -38,6 +38,12 @@ if($arrJson['events'][0]['message']['text'] == ""){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "รูปหรือสติกเกอร์";
+} else {
+$strUrl = "https://api.line.me/v2/bot/message/push";
+$arrPostData = array();
+$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
 }
 
 $ch = curl_init();
