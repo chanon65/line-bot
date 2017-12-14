2ec1214dfc52fec7cc1c8820409a514cbf92772e
chanon65 <pronteradap65@gmail.com>
2017-12-14 12:42:11 +0700

    1

--- bot.php
+++ bot.php
@@ -4,8 +4,8 @@ $strAccessToken = "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
  
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-//$strUrl = "https://api.line.me/v2/bot/message/push";
+//$strUrl = "https://api.line.me/v2/bot/message/reply";
+$strUrl = "https://api.line.me/v2/bot/message/push";
  
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
@@ -38,13 +38,11 @@ if($arrJson['events'][0]['message']['text'] == ""){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "รูปหรือสติกเกอร์";
-} elseif($arrJson['events'][0]['message']['text'] <> ""){
-$strUrl = "https://api.line.me/v2/bot/message/push";
+} 
 $arrPostData = array();
 $arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
 $arrPostData['messages'][0]['type'] = "text";
 $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
-}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
