3b94172f182bc5c34610b1cf201e306e5aed33d1
chanon65 <pronteradap65@gmail.com>
2017-12-14 14:06:55 +0700

    1

--- bot.php
+++ bot.php
@@ -45,6 +45,17 @@ $strUrl = "https://api.line.me/v2/bot/message/reply";
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
+
+
+if($arrJson['events'][0]['message']['text'] <> ""){
+
+$strUrl = "https://api.line.me/v2/bot/message/push";
+$arrPostData = array();
+$arrPostData['to'] = "U961224e379af4062d4ce99f7e9c46dfe";
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+
+}
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
