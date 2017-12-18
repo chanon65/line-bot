d7ecfd286a2756f184c53579c3301950bbed5e03
chanon65 <pronteradap65@gmail.com>
2017-12-18 09:06:39 +0700

    ASas

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ ob_start();
 $host = "192.168.1.15";
 $user = "sa";
 $pwd = "sapwd";
-$db = "hrfict"; 
+$db = "alarm_test"; 
 global $link;
 $link = array("Database"=>$db, "UID"=>$user , "PWD"=>$pwd , "MultipleActiveResultSets"=>true,"CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect( $host, $link);
