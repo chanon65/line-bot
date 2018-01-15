45d660200014fe93afc5964e027167e64bbeb04a
chanon65 <pronteradap65@gmail.com>
2018-01-15 15:31:16 +0700

    zxczxc

--- verify.php
+++ verify.php
@@ -1,3 +1,20 @@
 <?php
-
-
+{
+	"events" : [
+		{
+			"type" : "message",
+			"replyToken" : "2ObLFJCXF9ogLsCfrACIF3l98zCjCNWklcpA7Ic4C+nbM0qHi5fFxEoqQAxP6vUSRVm/4U5ShxjmjyR97THBsWz2fIU8RPTBuyxGk0IAfeW1eMgZ1a0H0rfYWQ5//k+tSIwOYvdKVkp8UkmsKKSDMQdB04t89/1O/w1cDnyilFU=",
+			"source" : {
+				"userId" : "U961224e379af4062d4ce99f7e9c46dfe",
+				"type" : "user"
+			},
+			"timestamp" : 1486930709120,
+			"message" : {
+				"type" : "text",
+				"id" : "5638..645..8",
+				"text" : "dddd"
+			}
+		}
+	]
+}
+?>
