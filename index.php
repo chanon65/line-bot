4deb8dda31a4c47de0e73474ecd73d8dc82f07ec
chanon65 <33611922+chanon65@users.noreply.github.com>
2019-04-23 16:38:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,12 +11,12 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+if($arrJson['events'][0]['message']['text'] == "Hello Dixell"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-} else if($arrJson['events'][0]['message']['text'] == "สวัสดี1"){
+} else if($arrJson['events'][0]['message']['text'] == "Hello Group Dixell"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
